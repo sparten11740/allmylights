849e18526e06fc6a4e2635f3a4da583b5350b248
Jan Wendland <jan.wendland@t-online.de>
2020-12-06 18:39:00 +0100

    Safely access sink/source configs

--- AllMyLights/Connectors/ConnectorFactory.cs
+++ AllMyLights/Connectors/ConnectorFactory.cs
@@ -19,8 +19,8 @@ namespace AllMyLights.Connectors.Sources
 
         public ISource[] GetSources()
         {
-            return Configuration.Sources.Mqtt
-                .Select((it) => new MqttSource(
+            return Configuration.Sources?.Mqtt
+                ?.Select((it) => new MqttSource(
                     options: it,
                     mqttClient: new MqttFactory().CreateMqttClient()))
                 .ToArray();
@@ -28,8 +28,8 @@ namespace AllMyLights.Connectors.Sources
 
         public ISink[] GetSinks()
         {
-            return Configuration.Sinks.OpenRgb
-                .Select((options) => new OpenRGBSink(options, (new OpenRGB.NET.OpenRGBClient(
+            return Configuration.Sinks?.OpenRgb
+                ?.Select((options) => new OpenRGBSink(options, (new OpenRGB.NET.OpenRGBClient(
                ip: options?.Server ?? "127.0.0.1",
                port: options?.Port ?? 6742,
                autoconnect: false).Wrap())))
